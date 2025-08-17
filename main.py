8a2f4838ed202f8b96806a525c1d125d89da9f5d
Sofia <sofia.arone@sou.inteli.edu.br>
2025-08-16 23:43:54 -0300

    feat: calculator project done

---
+++ main.py
@@ -0,0 +1,8 @@
+from mean_var_std import calculate
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+result = calculate(numbers)
+
+print("Calculation results:\n")
+for key, value in result.items():
+    print(f"{key} → {value}")
